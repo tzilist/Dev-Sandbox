525a6a6fb1c0d8863b4d08d2d68a2d6f1b9ecd41
Theodore Zilist <Theodore Zilist>
2016-02-17 09:01:27 -0800

    work in progress

--- server/server.js
+++ server/server.js
@@ -1,21 +1,62 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs-extra');
+var exec = require('child_process').exec, child;
 
-app.get('/',function(){
-  var exec = require('child_process').exec,
-    child;
-console.log('what')
- child = exec('npm install async',
- function (error, stdout, stderr) {
-     console.log('stdout: ' + stdout);
-     console.log('stderr: ' + stderr);
-     if (error !== null) {
+
+
+app.get('/sandbox', function(req, res, next) {
+  fs.copy(path.join(__dirname, './../client/template.html'), path.join(__dirname, './../client/rendered.html'), function(err) {
+      child = exec('bower install async --save', function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+
+        if (error !== null) {
           console.log('exec error: ' + error);
-     }
- });
-})
+        }
 
+        child = exec('grunt wiredep', function(error, stdout, stderr) {
+
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+          var options = {
+            headers: {
+              'Content-Type': 'text/html',
+              'charset': 'UTF-8'
+            }
+          };
+          res.status(200).sendFile(path.join(__dirname + './../client/rendered.html'), options, function() {
+            fs.remove(path.join(__dirname, './../client/rendered.html'), function(err) {
+              child = exec('bower uninstall jquery --save', function(error, stdout, stderr) {
+
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                  console.log('exec error: ' + error);
+                }
+              });
+            });
+
+
+          });
+        });
+      });
+    });
+
+
+
+});
+
+
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, './../client/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '../client')));
 
 
 
